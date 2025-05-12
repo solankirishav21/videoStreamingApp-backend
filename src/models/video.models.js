7685c7c4a846c6033070ec8991abd42adaf0cb91
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2025-05-13 00:27:54 +0530

    multer and cloudinary setup

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -41,4 +41,6 @@ const videoSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+videoSchema.plugin(mongooseAggregatePaginate);
+
 export const Video = mongoose.model("Video", videoSchema);
